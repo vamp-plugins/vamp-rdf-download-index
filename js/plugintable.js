fc64013a9669ddc54583758187ee027f61d6ed1b
Chris Cannam <cannam@all-day-breakfast.com>
2013-12-09 17:37:25 +0000

    Experiment towards dynamic Vamp plugin download list based on RDF descriptions

---
+++ js/plugintable.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+    
+    var parseRdf = function(data) {
+	
+	var parser = N3.Parser();
+	parser.parse(data,
+	             function (error, triple, prefixes) {
+        		 if (triple) {
+			     console.log(triple.subject, triple.predicate, triple.object, '.');
+			 }
+			 else {
+			     console.log("# That's all, folks!", prefixes)
+			 }
+		     });
+    };
+    
+    var parseIndex = function(data) {
+	lines = data.split("\n");
+	for (var i = 0; i < lines.length; i++) {
+	    var base = lines[i].replace(new RegExp("^.*/"), "");
+	    if (base === "" || base === lines[i]) {
+	    } else {
+		$.get("rdf/plugins/" + base + ".n3", parseRdf, "text");
+	    }
+	}
+    };
+
+    $.get("rdf/plugins/index.txt", parseIndex, "text");
+});
+
+
