c3bb61b42be05523e9e94951a4984d8754d631a0
Chris Cannam <cannam@all-day-breakfast.com>
2014-06-05 19:17:57 +0100

    Beginning to return some real data

--- yeti-spark/run.sh
+++ yeti-spark/run.sh
@@ -2,14 +2,20 @@
 #
 # This script is (I hope) just a stopgap
 
+mydir=`dirname "$0"`
+
 MAY_DIR=~/code/may
 YETI_DIR=~/code/yeti
 
-( cd spark ; mvn compile ) || exit 1
+if [ ! -f "spark/target/classes/spark/Spark.class" ]; then
+    ( cd spark ; mvn compile ) || exit 1
+fi
 
 CLASSPATH=`find ~/.m2 "$MAY_DIR" -name \*.jar -print0 | sed 's/\x00/:/g'`:"$YETI_DIR/yeti-lib.jar"
 
-"$MAY_DIR/bin/yc" -d target/classes -cp "spark/target/classes:$CLASSPATH" ./helloworld.yeti || exit 1
+"$MAY_DIR/bin/yc" -d target/classes -cp "spark/target/classes:$CLASSPATH" ./pluginlist.yeti || exit 1
 
-LD_LIBRARY_PATH="$MAY_DIR/ext/native/linux64:$LD_LIBRARY_PATH" java -cp target/classes:spark/target/classes:"$CLASSPATH" helloworld
+platform_tag=linux32
+
+VAMP_PATH=$mydir/../rdf/plugins LD_LIBRARY_PATH="$MAY_DIR/ext/native/$platform_tag:$LD_LIBRARY_PATH" java -cp target/classes:spark/target/classes:"$CLASSPATH" pluginlist
 
