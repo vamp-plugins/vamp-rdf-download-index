db8dd05d1053d3534e5e67d8554f9e7a98adee8c
Chris Cannam <cannam@all-day-breakfast.com>
2014-06-18 13:45:33 +0100

    Guess the arch

--- yeti-spark/run.sh
+++ yeti-spark/run.sh
@@ -15,7 +15,20 @@ CLASSPATH=`find ~/.m2 "$MAY_DIR" -name \*.jar -print0 | sed 's/\x00/:/g'`:"$YETI
 
 "$MAY_DIR/bin/yc" -d target/classes -cp "spark/target/classes:$CLASSPATH" ./pluginlist.yeti || exit 1
 
-platform_tag=linux32
+ARCH=unknown
 
-VAMP_PATH=$mydir/../rdf/plugins:$HOME/vamp LD_LIBRARY_PATH="$MAY_DIR/ext/native/$platform_tag:$LD_LIBRARY_PATH" java -cp target/classes:spark/target/classes:"$CLASSPATH" pluginlist
+case `uname -a` in
+Linux*x86_64*) ARCH=linux64;;
+Linux*) ARCH=linux32;;
+Darwin*) ARCH=osx;;
+CYG*) ARCH=win32;;
+MINGW*) ARCH=win32;;
+esac
+
+if [ "$ARCH" = "unknown" ]; then
+    echo "Failed to guess architecture for native libraries, sorry!" 1>&2
+    exit 2
+fi
+
+VAMP_PATH=$mydir/../rdf/plugins:$HOME/vamp LD_LIBRARY_PATH="$MAY_DIR/ext/native/$ARCH:$LD_LIBRARY_PATH" java -cp target/classes:spark/target/classes:"$CLASSPATH" pluginlist
 
