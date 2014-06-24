01f4efc44d8fbf35e65b635289b88925bbc26207
Chris Cannam <cannam@all-day-breakfast.com>
2014-06-24 15:40:08 +0100

    Remove dependencies on native code

--- run.sh
+++ run.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
-#
-# This script is (I hope) just a stopgap
 
 mydir=`dirname "$0"`
-
-MAY_DIR=~/code/may
-YETI_DIR=~/code/yeti
 
 #if [ ! -f "spark/target/classes/spark/Spark.class" ]; then
 #    ( cd spark ; mvn compile ) || exit 1
 #fi
 
-CLASSPATH=`echo jar/*.jar | sed 's/ /:/g'`:.
+export CLASSPATH="$mydir/target/classes":`echo jar/*.jar | sed 's/ /:/g'`
+export VAMP_PATH="$mydir/rdf/plugins"
 
-"$MAY_DIR/bin/yc" -d target/classes -cp "spark/target/classes:$CLASSPATH" ./pluginlist.yeti || exit 1
+# Compile the app!
+java yeti.lang.compiler.yeti -d target/classes pluginlist.yeti || exit 1
 
-ARCH=unknown
-
-case `uname -a` in
-Linux*x86_64*) ARCH=linux64;;
-Linux*) ARCH=linux32;;
-Darwin*) ARCH=osx;;
-CYG*) ARCH=win32;;
-MINGW*) ARCH=win32;;
-esac
-
-if [ "$ARCH" = "unknown" ]; then
-    echo "Failed to guess architecture for native libraries, sorry!" 1>&2
-    exit 2
-fi
-
-VAMP_PATH=$mydir/rdf/plugins LD_LIBRARY_PATH="$MAY_DIR/ext/native/$ARCH:$LD_LIBRARY_PATH" java -cp target/classes:spark/target/classes:"$CLASSPATH" pluginlist
+# Run the app!
+java pluginlist
 
