ff64f1d5331adcaa76e2bd1e2245e0b2a5982a3a
Chris Cannam <cannam@all-day-breakfast.com>
2014-06-25 11:16:59 +0100

    Move scripts to subdir, add simple init script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mydir=`dirname "$0"`
+rootdir=$(dirname "$0")/..
 
 #if [ ! -f "spark/target/classes/spark/Spark.class" ]; then
 #    ( cd spark ; mvn compile ) || exit 1
 #fi
 
-# NB path must contain . so that Spark will serve static files from public/
-export CLASSPATH="$mydir/target/classes:.:"`echo jar/*.jar | sed 's/ /:/g'`
-export VAMP_PATH="$mydir/rdf/plugins"
+# NB path must contain rootdir so that Spark will serve static files from public/
+export CLASSPATH="$rootdir/target/classes:$rootdir:"`echo jar/*.jar | sed 's/ /:/g'`
+export VAMP_PATH="$rootdir/rdf/plugins"
 
 # Compile the app!
 java yeti.lang.compiler.yeti -d target/classes pluginlist.yeti || exit 1
---
+++ scripts/vamp-spark-init.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# A simple init script
+
+#rootdir=/var/www/vamp-plugins.org
+rootdir=/home/cannam/code/vamp-rdf-download-index
+
+logdir="$rootdir/log"
+logfile="$logdir/spark.log"
+
+find_pid() {
+    ps auxw | grep 'java pluginlist' | grep -v grep | awk '{ print $2; }'
+}
+
+start() {
+    if [ ! -d "$logdir" ]; then
+	if [ -d "$rootdir" ]; then
+	    mkdir -p "$logdir" || return 1
+	else
+	    echo "Root dir $rootdir not found" ; return 1
+	fi
+    fi
+    ( cd "$rootdir" && ./scripts/run.sh >> "$logfile" 2>&1 & ) || return 1
+}    
+
+stop() {
+    pid=$(find_pid)
+    if [ -n "$pid" ]; then
+	kill -HUP "$pid" && echo "Killed pid $pid"
+    else
+	echo "Failed to find pid of running app" ; return 1
+    fi
+}
+
+case $1 in
+
+    start)
+	echo "Starting Vamp Spark app..."
+	pid=$(find_pid)
+	if [ -n "$pid" ]; then 
+	    echo "Seems to be already running, with pid $pid"; exit 1;
+	fi
+	start && echo "(Check $logfile for logs)" && echo Done;;
+
+    stop)
+	echo "Stopping Vamp Spark app..."
+	stop && echo Done;;
+
+    restart)
+	echo "Restarting Vamp Spark app..."
+	stop
+	start && echo "(Check $logfile for logs)" && echo Done;;
+
+    *)
+	echo "Usage: $0 {start|stop|restart}"
+	exit 2;;
+
+esac;
+
+	    
