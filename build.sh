2ae6f7bfe2aea85ea74af775d366fc61e083463e
Chris Cannam <cannam@all-day-breakfast.com>
2020-05-15 11:40:43 +0100

    Update dependencies, and include as repoint subrepos

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu
+
+./repoint install
+
+( cd yeti ; ant && cp *.jar ../jar && rm asm*.jar )
+( cd yertle ; ant && cp yertle.jar ../jar && cp yertle.jar ../may/ext/jar/ )
+( cd jvamp ; make jvamp.jar && cp jvamp.jar ../jar && cp jvamp.jar ../may/ext/jar )
+( cd may ; ant && cp may.jar ../jar )
+
+echo "Dependencies built, now run scripts/run.sh to run in test environment"
